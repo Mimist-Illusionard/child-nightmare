c30b99eb4d0b71f543a59d7ec3571984f9d64fe5
RestartYC <69119185+RestartYC@users.noreply.github.com>
2021-03-20 17:05:50 +0300

    SoundCont

---
+++ ScreamGameJam/ScreamGameJam/Assets/scripts/AudioSettings.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    private GameObject Player;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        source = GetComponent<AudioSource>();
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<AuDIOController>().audios.Add(this);
+    }
+
+    public void AllVoids()
+    {
+        WallsError();
+    }
+
+    private void WallsError()
+    {
+        RaycastHit hit;
+        if (Physics.Linecast(gameObject.transform.position,Player.transform.position,out hit))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                source.minDistance = 1f;
+            }
+            else
+            {
+                source.minDistance = 0.2f;
+            }
+        }
+    }
+}
