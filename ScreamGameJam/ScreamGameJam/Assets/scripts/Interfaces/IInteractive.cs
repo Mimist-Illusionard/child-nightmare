d5246f2b37a7b7e846f422591ac0fb05b1585909
Illusion <canada190@mai.ru>
2022-02-20 20:18:36 +0300

    Remove: - Unused scripts

--- ScreamGameJam/ScreamGameJam/Assets/scripts/Interfaces/IInteractive.cs
+++
@@ -1,7 +0,0 @@
-﻿public interface IInteractive
-{
-    bool IsInteractiveByPlayer { get; set; }
-
-    void InteractLogic();
-    void BlockedInteractLogic();
-}
--- ScreamGameJam/ScreamGameJam/Assets/scripts/Interfaces/ILogic.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface ILogic
-{
-    void Logic();
-}
