55a283cb7dfb2ea39bad9b646f407cae35b42b9c
Illusion <canada190@mai.ru>
2022-02-20 20:19:11 +0300

    Add: - Ruinum.Core scripts

---
+++ ScreamGameJam/ScreamGameJam/Assets/Scripts/Ruinum.Core/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+
+public class GameManager : BaseSingleton<GameManager>
+{
+    private List<IExecute> _executes = new List<IExecute>();
+
+    private void Update()
+    {
+        for (var i = 0; i < _executes.Count; i++)
+        {
+            var updatable = _executes[i];
+            updatable.Execute();
+        }
+    }
+
+    public void AddExecuteObject(IExecute executeGameObject)
+    {
+        _executes.Add(executeGameObject);
+    }
+
+    public void RemoveExecuteObject(IExecute executeGameObject)
+    {
+        _executes.Remove(executeGameObject);
+    } 
+
+    public void ClearAllExecuteObjects()
+    {
+        _executes.Clear();
+    }
+
+    private void OnDestroy()
+    {
+        ClearAllExecuteObjects();
+    }  
+}
