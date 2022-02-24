82475a8685982dbd6a0cb47eb947cd406f343840
Illusion <canada190@mai.ru>
2022-02-24 19:31:08 +0300

    Add: - UiScreens for endings - Act_2 events
    Change:
    - Location_Clone scene

--- ScreamGameJam/ScreamGameJam/Assets/Scripts/TeleportSystem.cs
+++ ScreamGameJam/ScreamGameJam/Assets/Scripts/TeleportSystem.cs
@@ -9,6 +9,7 @@ public class TeleportSystem : MonoBehaviour
     public void RefreshPlayerPosition()
     {
         Player.GetComponent<CharacterController>().enabled = false;
+        Player.gameObject.transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
         Player.transform.position = SpawnPoint.position;
         Player.GetComponent<CharacterController>().enabled = true;
     }
